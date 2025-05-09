23172780934215ae84caacea6e2d894e5244621d
John Lam <jlam@iunknown.com>
2025-05-09 15:52:46 -0700

    ref: removing unneeded files

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from ai-app-starter-postgres!")
-
-
-if __name__ == "__main__":
-    main()
--- set_region.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export AZURE_DEPLOY_REGION="eastus2"
-echo "AZURE_DEPLOY_REGION=eastus2"
